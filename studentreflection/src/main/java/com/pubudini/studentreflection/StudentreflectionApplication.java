9c6a06f19dc3a062e733674097ebaded6b8cd899
pubudini-rathnayake <pubudini-rathnayake-pubudinithamali@gmail.com>
2026-05-31 10:01:31 +0530

    Initial Spring Boot backend setup

---
+++ studentreflection/src/main/java/com/pubudini/studentreflection/StudentreflectionApplication.java
@@ -0,0 +1,13 @@
+package com.pubudini.studentreflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentreflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentreflectionApplication.class, args);
+	}
+
+}
